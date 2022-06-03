f78b02fac62d2a2abb06d244b10981bc3e2a3f64
Hai Lee <duonghailee@gmail.com>
2022-06-03 09:30:42 +0700

    Add more tests and fix Annotation response tests

--- test.py
+++ test.py
@@ -46,7 +46,8 @@ class TestResponse(unittest.TestCase):
         response = requests.post(self.base_url,
                                  headers=self.headers,
                                  data=self.payload).json()['response']
-        self.assertEqual(response.get('type'), 'texts')
+
+        self.assertEqual(response.get('type'), 'annotations')
 
     def test_api_response_when_no_given_param(self):
         """API should work with default pipeline
@@ -57,7 +58,7 @@ class TestResponse(unittest.TestCase):
         payload = json.dumps(payload_dict)
         response = requests.post(self.base_url,
                                  headers=self.headers,
-                                 data=payload).json()['response']['texts'][0]
+                                 data=payload).json()['response']
 
         self.assertIn('errs', response['annotations'])
 
@@ -65,7 +66,6 @@ class TestResponse(unittest.TestCase):
             self.assertIn(_, response['annotations']['errs'][0])
 
         for prop in ['original', 'type', 'explanation', 'suggestion']:
-
             self.assertIn(prop, response['annotations']['errs'][0]['features'])
 
     def test_api_response_content(self):
@@ -82,9 +82,9 @@ class TestResponse(unittest.TestCase):
                 "content": 'Prošeatadoarjagat'
             }
             payload = json.dumps(payload_dict)
-            response = requests.post(
-                self.base_url, headers=self.headers,
-                data=payload).json()['response']['texts'][0]
+            response = requests.post(self.base_url,
+                                     headers=self.headers,
+                                     data=payload).json()['response']
 
             self.assertIn('errs', response['annotations'])
 
@@ -92,7 +92,6 @@ class TestResponse(unittest.TestCase):
                 self.assertIn(_, response['annotations']['errs'][0])
 
             for prop in ['original', 'type', 'explanation', 'suggestion']:
-
                 self.assertIn(prop,
                               response['annotations']['errs'][0]['features'])
 
@@ -112,7 +111,7 @@ class TestResponse(unittest.TestCase):
         payload = json.dumps(payload_dict)
         response = requests.post(self.base_url,
                                  headers=self.headers,
-                                 data=payload).json()['response']['texts'][0]
+                                 data=payload).json()['response']
 
         self.assertIn('errs', response['annotations'])
 
@@ -120,7 +119,6 @@ class TestResponse(unittest.TestCase):
             self.assertIn(_, response['annotations']['errs'][0])
 
         for prop in ['original', 'type', 'explanation', 'suggestion']:
-
             self.assertIn(prop, response['annotations']['errs'][0]['features'])
 
     def test_api_response_with_too_large_req(self):
@@ -179,14 +177,13 @@ class TestResponse(unittest.TestCase):
         """
 
         for chr in self.CONTROL_CHARS:
-
-            content = self.base_text[0] + chr
+            content = self.base_text[0][:5] + chr + self.base_text[0][5:]
             payload_dict = {"type": "text", "content": content}
             payload = json.dumps(payload_dict)
             response = requests.post(self.base_url,
                                      headers=self.headers,
                                      data=payload).json()
-
+            print(response)
             self.assertIn('failure', response)
             self.assertEqual(response['failure']['errors'][0]['code'],
                              'elg.request.invalid')
@@ -211,6 +208,42 @@ class TestResponse(unittest.TestCase):
                          'elg.request.invalid')
         self.assertEqual(response['failure']['errors'][0]['detail']['text'],
                          'There is too many white space in the content')
+
+    def test_api_response_with_correct_content_as_in_original(self):
+        """API should return correct content with given input
+        from example 1 from the paper 
+        https://ep.liu.se/ecp/168/008/ecp19168008.pdf
+        Or it's in `test.txt`
+        Found errors should contain atleast: Norgag -> Norgga
+        dáid -> dáin
+        iskkadan bargguin should be iskkadanbargguin (can't be found)
+        extra space after bargguin.
+        The original return from se.zip is in `test.json`
+        """
+        content = "Oktiibuot 13 Norgag doaktára leat leamaš mielde dáid iskkadan bargguin ."
+        payload_dict = {"type": "text", "content": content}
+        payload = json.dumps(payload_dict)
+        response = requests.post(self.base_url,
+                                 headers=self.headers,
+                                 data=payload).json()['response']
+
+        self.assertIn('errs', response['annotations'])
+        with open('test.json', 'r') as f:
+            err_orig = json.load(f)
+
+        err_lst_org = err_orig['errs']
+        err_lst = response['annotations']['errs']
+
+        self.assertEqual(err_orig['text'], content)
+        self.assertEqual(len(err_lst), len(err_lst_org))
+
+        for err, err_elg in zip(err_lst_org, err_lst):
+            self.assertEqual(err[0], err_elg['features']['original'])
+            self.assertEqual(err[1], err_elg['start'])
+            self.assertEqual(err[2], err_elg['end'])
+            self.assertEqual(err[3], err_elg['features']['type'])
+            self.assertEqual(err[4], err_elg['features']['explanation'])
+            self.assertEqual(err[5][0], err_elg['features']['suggestion'][0])
 
 
 if __name__ == '__main__':
