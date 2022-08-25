fcfe4a073f870f4544a05ed5ec8a44a6d74a6f43
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-08-25 13:51:41 +0300

    Simplify the code
    Remove extra checks for text and params.

--- app.py
+++ app.py
@@ -1,79 +1,31 @@
-import utils
 from elg import FlaskService
 from elg.model import Failure
 from elg.model import TextsResponse, TextRequest
 from elg.model.base import StandardMessages
 
-import re
+import utils
 
 
 class SamiChecker(FlaskService):
 
-    available_pipes = ['smegramrelease', 'smegram']
-    MAX_CHAR = 4095
-    MIN_CHAR = 2
-    CONTROL_CHARS = ['\r', '\n', '\t', '\u2028', '\u2029']
-
-    def check_params(self, request):
-        params = request.params
-        default_pipe = self.available_pipes[1]
-        if not params:
-            return default_pipe
-        if 'pipe' not in params:
-            return default_pipe
-        pipe = params['pipe']
-        if pipe not in self.available_pipes:
-            return False
-        return pipe
-
-    def check_text(self, text):
-        text = text.strip()
-        if len(text) < self.MIN_CHAR:
-            tooShortMessage = StandardMessages.generate_elg_request_invalid(
-                detail={'text': 'lower limit is 2 characters in length'})
-            return False, Failure(errors=[tooShortMessage])
-        elif len(text) > self.MAX_CHAR:
-            tooLargeMessage = StandardMessages.generate_elg_request_too_large()
-            return False, Failure(errors=[tooLargeMessage])
-        elif any(c in self.CONTROL_CHARS for c in text):
-            inputHasControlCharMessage = StandardMessages.generate_elg_request_invalid(
-                detail={'text': 'There is control character in the content'})
-            return False, Failure(errors=[inputHasControlCharMessage])
-        elif re.search(r'(\s\s+)', text):
-            inputHasExtraWhiteSpaceMessage = StandardMessages.generate_elg_request_invalid(
-                detail={
-                    'text': 'There is too many white space in the content'
-                })
-            return False, Failure(errors=[inputHasExtraWhiteSpaceMessage])
-        else:
-            return True, text
+    MAX_CHAR = 3750
 
     def process_text(self, request: TextRequest):
 
         text = request.content
-        text_ok, text_check_res = self.check_text(text)
-        if not text_ok:
-            return text_check_res
-
-        pipe = self.check_params(request)
-        if not pipe:
-            # missing parameter
-            detail = {
-                'params':
-                f'invalid params, support pipes are: {self.available_pipes}'
-            }
-            missingRequestMsg = StandardMessages.generate_elg_request_missing(
-                detail=detail)
-            return Failure(errors=[missingRequestMsg])
+        if len(text) > self.MAX_CHAR:
+            error = StandardMessages.generate_elg_request_too_large()
+            return Failure(errors=[error])
 
         try:
+            pipe = "smegram"
             res = utils.gmr_func_elg(text, pipe)
+            return res
         except Exception as err:
             error = StandardMessages.generate_elg_service_internalerror(
                 params=[str(err)])
             return Failure(errors=[error])
-        return res
 
 
-flask_service = SamiChecker("sami-grm-checker")
+flask_service = SamiChecker("sami-checker")
 app = flask_service.app
